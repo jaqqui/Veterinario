324cc6ff323efad588e87c88c0d37bad8e6ea5ee
xp <xp@CETYSDAM031>
2018-04-20 17:35:18 +0200

    hola amigo

---
+++ src/codigo/Veterinario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author Jiaqi
+ */
+public class Veterinario {
+    public String nombre="";
+    public String raza="";
+     public String sexo="";
+      public String fechaN="";
+     public String chip="";
+        public String especie="";
+    
+}
